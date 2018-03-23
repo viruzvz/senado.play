238aab9dfe885ffbb78e93f5b54b8a0187c8f1b5
tiagov <tiagov@senado.gov.br>
2018-03-23 16:54:39 -0300

    Implementando o click para o stick menu

--- src/js/components/contentMiddle.js
+++ src/js/components/contentMiddle.js
@@ -4,38 +4,38 @@ import NavSec from './nav.js';
 import Cards from './card.js';
 import { StickyContainer, Sticky } from 'react-sticky';
 import ScrollableAnchor from 'react-scrollable-anchor';
+import $ from 'jquery'
 
 export default class ContentMiddle extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.initEvents()
+	}
+
+	initEvents() {
+		$(document.body).on('click', '.menuStick-link', this.navBarClick)
+	}
+
+	navBarClick(e) {
+		e.preventDefault();
+		//$(document).off("scroll") TODO:Terminar função baseada no link:
+		//https://jsfiddle.net/cse_tushar/Dxtyu/141/
+
+		$('.menuStick-link').removeClass('active')
+		debugger
+	}
+
   render() {
     return (
 		<div className='bg-gray-dark'>
-			<StickyContainer>
-				<Sticky>
-		          {
-		            ({
-		              style,
-		              isSticky,
-		              wasSticky,
-		              distanceFromTop,
-		              distanceFromBottom,
-		              calculatedHeight
-		            }) => {
-		              return (
-		              	<header className='bg-gray-dark py-3 zindex-fixed' style={style}>
-		              	<Container>
-		              		<Row>
-		                	<NavSec></NavSec>
-		                	</Row>
-		                </Container>
-		                </header>
-		              )
-		            }
-		          }
-				</Sticky>
 			<Container>
-				<ScrollableAnchor id={'section1'}>
-					<div>
-					<Row className='mb-4'>
+				<Row>
+				<NavSec></NavSec>
+				</Row>
+			</Container>
+			<Container>
+					<Row className='mb-4' id="section1">
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=402' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=401' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simplesmente uma simulação de texto da indústria ti XVI,'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=400' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,'></Cards></Col>
@@ -45,18 +45,16 @@ export default class ContentMiddle extends React.Component {
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=404' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simple de impressos, e vem sendo utilizado desde o século XVI,'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=405' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,'></Cards></Col>
 					</Row>
-			        <Row className='text-center'>
-						<Col><Button outline color="secondary">Ver Mais</Button>{' '}</Col>
-			        </Row>
-			        </div>
-		        </ScrollableAnchor>
 
-		        <ScrollableAnchor id={'section2'}>
-		        	<div>
-			        <Row className='mb-4 text-left'>
+			    <Row className='text-center'>
+						<Col><Button outline color="secondary">Ver Mais</Button>{' '}</Col>
+			    </Row>
+			    
+					<Row className='mb-4 text-left' >
 						<Col><p className='border-bottom white m-0 pb-2'>BODY MENU</p></Col>
-			        </Row>
-					<Row className='mb-4'>
+			    </Row>
+
+					<Row className='mb-4' id="section2">
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=406' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simpleem sendo utilizado desde o século XVI,'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=407' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simplesmente uma simulação de teutilizado desde o século XVI,'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=408' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simplesmente uma simulação de texto da izado desde o século XVI,'></Cards></Col>
@@ -66,18 +64,15 @@ export default class ContentMiddle extends React.Component {
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=410' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográdesde o século XVI,'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/318/180?image=411' timer cardbody title='Meu Titulo' subtitle='Meu subtitle' text='Lorem Ipsumográfica e de impressos, e vem sendo utilizado desde o século XVI,'></Cards></Col>
 					</Row>
-			        <Row className='text-center'>
+			    <Row className='text-center'>
 						<Col><Button outline color="secondary">Ver Mais</Button>{' '}</Col>
-			        </Row>
-			        </div>
-		        </ScrollableAnchor>
+			    </Row>
 
-		        <ScrollableAnchor id={'section3'}>
-		        	<div>
-			        <Row className='mb-4 text-left'>
+			    <Row className='mb-4 text-left'>
 						<Col><p className='border-bottom white m-0 pb-2'>BODY MENU</p></Col>
-			        </Row>
-					<Row className='mb-4'>
+			    </Row>
+
+					<Row className='mb-4' id="section3">
 						<Col className='cut'><Cards src='https://unsplash.it/118/200?image=399'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/118/200?image=398'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/118/200?image=397'></Cards></Col>
@@ -85,14 +80,12 @@ export default class ContentMiddle extends React.Component {
 						<Col className='cut'><Cards src='https://unsplash.it/118/200?image=395'></Cards></Col>
 						<Col className='cut'><Cards src='https://unsplash.it/118/200?image=393'></Cards></Col>
 					</Row>
-			        <Row className='text-center'>
+
+			    <Row className='text-center'>
 						<Col><Button outline color="secondary">Ver Mais</Button>{' '}</Col>
-			        </Row>
-			        </div>
-		        </ScrollableAnchor>
+			    </Row>
 
 			</Container>
-			</StickyContainer>
 		</div>
     );
   }
