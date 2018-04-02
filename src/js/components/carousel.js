d035d802d9a898018b0d96fb7f457be035c586d6
Carlos Neri <viruzvz@gmail.com>
2018-04-02 11:25:24 -0300

    carousel largura fotos

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -3,17 +3,17 @@ import { UncontrolledCarousel } from 'reactstrap';
 
 const items = [
   {
-    src: 'https://picsum.photos/1400/500/?random',
+    src: 'https://picsum.photos/1800/500/?random',
     altText: 'Slide 1',
     caption: 'Slide 1'
   },
   {
-    src: 'https://picsum.photos/1400/500/?image=167',
+    src: 'https://picsum.photos/1800/500/?image=167',
     altText: 'Slide 2',
     caption: 'Slide 2'
   },
   {
-    src: 'https://picsum.photos/1400/500/?image=345',
+    src: 'https://picsum.photos/1800/500/?image=345',
     altText: 'Slide 3',
     caption: 'Slide 3'
   }
