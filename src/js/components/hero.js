6031cfd6a541477d0c3eba72088f87c38548915f
Carlos Eduardo Gomes Neri <cegn@senado.leg.br>
2018-05-08 22:35:11 -0300

    fotos senadores

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -4,7 +4,7 @@ import LazyHero from 'react-lazy-hero';
 export default class Hero extends React.Component {
   render() {
     return (
-	    <LazyHero imageSrc="https://unsplash.it/2000/1300?random" minHeight='60vh' parallaxOffset={100} isCentered={false} opacity={0.3}>
+	    <LazyHero imageSrc="../../assets/senadores/20180404_03336mo.jpg" minHeight='60vh' parallaxOffset={100} isCentered={false} opacity={0.3}>
 	        <div className='bg-gradient'>
 	            <div className='container'>
 	                <h1 className='shadow white size--large'>{this.props.headertitle}</h1>
